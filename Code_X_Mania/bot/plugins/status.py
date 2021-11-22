d970da46cd445da7027a74747719c460e370b225
Code X Mania <urluploaderadarsh@gmail.com>
2021-11-22 10:08:05 +0530

    Create status.py

---
+++ Code_X_Mania/bot/plugins/status.py
@@ -0,0 +1,27 @@
+import shutil, psutil
+from bot import botStartTime
+from utils_bot import *
+from pyrogram import Client as app
+
+@app.on_message(filters.private & filters.command(["stats","status"]))
+async def stats(bot, update):
+  currentTime = readable_time((time.time() - botStartTime))
+  total, used, free = shutil.disk_usage('.')
+  total = get_readable_file_size(total)
+  used = get_readable_file_size(used)
+  free = get_readable_file_size(free)
+  sent = get_readable_file_size(psutil.net_io_counters().bytes_sent)
+  recv = get_readable_file_size(psutil.net_io_counters().bytes_recv)
+  cpuUsage = psutil.cpu_percent(interval=0.5)
+  memory = psutil.virtual_memory().percent
+  disk = psutil.disk_usage('/').percent
+  botstats = f'<b>Bot Uptime:</b> {currentTime}\n' \
+            f'<b>Total disk space:</b> {total}\n' \
+            f'<b>Used:</b> {used}  ' \
+            f'<b>Free:</b> {free}\n\n' \
+            f'📊Data Usage📊\n<b>Upload:</b> {sent}\n' \
+            f'<b>Down:</b> {recv}\n\n' \
+            f'<b>CPU:</b> {cpuUsage}% ' \
+            f'<b>RAM:</b> {memory}% ' \
+            f'<b>Disk:</b> {disk}%'
+  await update.reply_text(botstats)
